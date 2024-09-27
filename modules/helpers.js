36ff0d494bcb7499dc95046b0024c029821408a2
omer <aho700@gmail.com>
2024-09-27 23:36:34 +0200

    added uuidv4 generator function

--- modules/helpers.js
+++ modules/helpers.js
@@ -91,6 +91,14 @@ function get_style_rule_value(selector, style) {
   }
 }
 
+// UUID
+function generateUuidv4() {
+  return "10000000-1000-4000-8000-100000000000".replace(/[018]/g, c =>
+    (+c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> +c / 4).toString(16)
+  );
+}
+
+
 export {
   el,
   digToRad,
@@ -102,5 +110,5 @@ export {
   get_style_rule_value,
   cursorHitTest,
   isIntersect,
-
+  generateUuidv4,
 };
